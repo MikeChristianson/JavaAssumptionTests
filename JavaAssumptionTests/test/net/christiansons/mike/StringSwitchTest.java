d5c33da8b823f6dabbd664f3bf91ad89601272be
Mike Christianson <mike@codeaweso.me>
2012-08-16 12:05:56 -0700

    Java's new ability to switch on a String prompted a discussion on whether or not a null String would end up executing the default block. It does not; a NullPointerException is thrown at switch().

---
+++ JavaAssumptionTests/test/net/christiansons/mike/StringSwitchTest.java
@@ -0,0 +1,30 @@
+package net.christiansons.mike;
+
+import org.junit.Test;
+
+public class StringSwitchTest {
+
+	private static final String NULL = null;
+
+	@Test(expected=NullPointerException.class)
+	public void testSwitchOnNullString() {
+		getGreeting(NULL);
+	}
+
+	public String getGreeting(String string) {
+		String greeting;
+		switch (string) {
+		case "hello":
+			greeting = "hello";
+			break;
+		case "hi":
+			greeting = "howdy";
+			break;
+		default:
+			greeting = "unknown";
+			break;
+		}
+		return greeting;
+	}
+
+}
